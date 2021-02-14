771c3dab3887ae64fd9f8f6cf826e1d18061f2f9
Pravin J <pravinherester@gmail.com>
2021-02-14 10:08:50 +0000

    LeapYear

--- ConditonalBlock/leapyear.py
+++ ConditonalBlock/leapyear.py
@@ -1,14 +1,13 @@
-print("Leap Year Program")
+print("Leap Year Prograaaam")
 year=int(input ("Enter the Year"))
 if year%4:
-     print(f"{year} is a leap year")
-else:
      if year%100:
          if year%400:
             print(f"{year} is a leap year")
          else:
             print(f"{year} is not a leap year")
      else:
-         print(f"{year} is not a leap year")
-
-
+         print(f"{year} is  a leap year")
+else:         
+ print(f"{year} is not a leap year")
+         
--- main.py
+++ main.py
@@ -14,5 +14,3 @@ if year%4 ==0:
           print(f"{year} is a leap year")
 else:
     print(f"{year} is not a leap year")
-
-
