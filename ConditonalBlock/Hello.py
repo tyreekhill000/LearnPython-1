330ed88e5bb52108f8e5a5896deb0e21b63ee60a
Jagadeesan <pjagadeesan@paypal.com>
2021-02-14 15:55:10 +0530

    IfELSE

---
+++ ConditonalBlock/Hello.py
@@ -0,0 +1,11 @@
+#My first proram
+
+print("Hello World");
+print("what is your name");
+myname=input();
+print( "hi " + myname );
+print("Length of your name is");
+print(len(myname));
+print("what is your age");
+myage=input();
+print("your age will be " + str(int(myage) +1) +"next year");
