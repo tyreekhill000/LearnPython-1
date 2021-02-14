ce54ce4bc3541793186dd933844dd0f2294c9284
Pravin J <pravinherester@gmail.com>
2021-02-14 10:46:25 +0000

    Odd or Even

--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-print("Leap Year Program")
-year=int(input ("Enter the Year"))
-year_four=year%4
-year_100=year%100
-year_400=year%400
-print(f"year/4=={year_four} ,Year_100=={year_100}, Year_400={year_400}")
-if year%4 ==0:
-     if year%100==0:
-         if year%400 ==0:
-            print(f"{year} is a leap year")
-         else:
-            print(f"{year} is not a leap year")
-     else:
-          print(f"{year} is a leap year")
-else:
-    print(f"{year} is not a leap year")
+#if else
+# if condition:
+#     do some code
+#  else:
+#     do some code
+
+# print("Welcome to tthe roller coaster")
+# int_height=int(input("what is your height\n")) # converting Strint to int
+# #Note the collon used after if conditon
+# if (int_height>120):  # checking if the height is greater than 120 cm
+#     print("You are allowed") # Note the Indentation 
+# else: #Note the collon used after if conditon
+#     print("You are not allowed")# Note the Indentation 
+
+#Odd or even 
+# print("Odd or Even")
+# number=int(input ("Enter the number "))
+# if(number%2==0):
+#     print(f"{number} is even number")
+# else:
+#     print(f"{number} is odd number")
+
+#nested if else
+
+# if condition:
+#     if condition:
+#       do some code
+#     else:
+#       do some code 
+# else:
+#     do some code 
+
+# print("Welcome to tthe roller coaster")
+# int_height=int(input("what is your height\n")) # converting Strint to int
+# int_age=int(input("What is your age\n"))
+# #Note the collon used after if conditon
+# if (int_height>120):  # checking if the height is greater than 120 cm
+#     print("You are allowed") # Note the Indentation 
+#     if(int_age >18):
+#         print("price == 10")
+#     else:
+#          print("price == 5")
+# else: #Note the collon used after if conditon
+#     print("You are not allowed")# Note the Indentation 
+
+   # if /elif/else
+
+   # if condition:
+#     if condition:
+#       do some code
+#    elif condition:
+#       do some code
+#    else:
+#       do some code 
+# else:
+#     do some code 
+
+
+print("Welcome to the roller coaster")
+int_height=int(input("what is your height\n")) # converting Strint to int
+int_age=int(input("What is your age\n"))
+#Note the collon used after if conditon
+if (int_height>120):  # checking if the height is greater than 120 cm
+    print("You are allowed") # Note the Indentation 
+    if(int_age <12 ): #Note the collon used after if conditon .. Use Conditional operator >=
+        print("price == 5")
+    elif(int_age<=18): #Note the collon used after elif conditon .. Used Conditional operator >= and<
+        print("price == 8")
+    else:
+         print("price == 10")
+else: #Note the collon used after if conditon
+    print("You are not allowed")# Note the Indentation
